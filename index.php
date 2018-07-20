4f2b0723221553411616daf6d03e594c1ed0a56a
josh-a-coder <joshuacireap@hotmail.com>
2018-07-20 10:19:28 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+  include_once 'header.php';
+  ?>
+      <div class="container">
+        
+          <div class="row">
+
+            <div class="col-md-6 col-sm-6">
+              <center><b>LANDCRUISER 200 SERIES</b></center>
+            </div>
+
+            <div class="col-md-6">
+              <center><img src="images/lc200.jpeg" /></center>
+            </div>
+
+          </div>
+
+      </div>
+
+
+<?php
+  include_once 'footer.php';
+?>
